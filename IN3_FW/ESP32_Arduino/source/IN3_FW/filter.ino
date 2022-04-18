63683d370e09bbc532dc1cbd2d3b2ae50b6687e3
unknown <pablosanchez18393@hotmail.com>
2022-04-18 18:39:04 +0200

    changed filter to 0.8hz

--- IN3_FW/ESP32_Arduino/source/IN3_FW/filter.ino
+++ IN3_FW/ESP32_Arduino/source/IN3_FW/filter.ino
@@ -1,5 +1,7 @@
-const double b[] = {0.00024132, 0.00048264, 0.00024132};
-const double a[] = {1.95558189, -0.95654717};
+//butterworth2 0.8hz cutoff frequency, 1khz sampling, 2nd order, low pass
+
+const double b[] = {0.000006294162241, 0.00001258832448, 0.000006294162241};
+const double a[] = {1.9928914235, -0.9929165938};
 
 double butterworth2(double y1, double y2, double x0, double x1, double x2) {
   return (a[0] * y1 + a[1] * y2 +
